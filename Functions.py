54d3ea57f26aeaddae58856a88bb2b35ec375fb5
Fatehsandhu <fatehsandhu@gmail.com>
2017-11-29 14:49:33 -0500

    First empty

--- Functions.py
+++ Functions.py
@@ -5,22 +5,5 @@ import hashlib
 
 path = "/Users/fatehsandhu/test.txt"
 
-last = path.split('/')
-last = last[-1]
-
-print last
-
-print os.path.getsize(path)
-
-print("Sha 1 Digest")
-
-hash_object = hashlib.sha1(path)
-hex_dig = hash_object.hexdigest()
-print(hex_dig)
-
-print("MD5 Digest")
-
-hash_object = hashlib.md5(b'Hello World')
-print(hash_object.hexdigest())
 
 print "End of functions"
