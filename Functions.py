d9d898979f95a6091dc442d388554217c8023309
Fatehsandhu <fatehsandhu@gmail.com>
2017-11-29 14:35:56 -0500

    FIrst

---
+++ Functions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import os
+import hashlib
+
+
+path = "/Users/fatehsandhu/test.txt"
+
+last = path.split('/')
+last = last[-1]
+
+print last
+
+print os.path.getsize(path)
+
+print("Sha 1 Digest")
+
+hash_object = hashlib.sha1(path)
+hex_dig = hash_object.hexdigest()
+print(hex_dig)
+
+print("MD5 Digest")
+
+hash_object = hashlib.md5(b'Hello World')
+print(hash_object.hexdigest())
+
+print "End of functions"
