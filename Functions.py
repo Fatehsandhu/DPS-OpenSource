b82a012bce128a28f5cbdd8dc82b93877d55a2f1
Fatehsandhu <fatehsandhu@gmail.com>
2017-11-29 23:43:12 -0500

    Final

--- Functions.py
+++ Functions.py
@@ -5,26 +5,27 @@ import hashlib
 
 path = "/Users/fatehsandhu/test.txt"
 
-last = path.split('/')
-last = last[-1]
-
-print last
+def fileName():
+    last = path.split('/')
+    last = last[-1]
+    print last
 
 # Gets the File Size
-print os.path.getsize(path)
+def fileSize():
+    print os.path.getsize(path)
 
 
 # The Hash Functions
-print("Sha 1 Digest")
+def SHA():
+    print("Sha 1 Digest")
+    hash_object = hashlib.sha1(path)
+    hex_dig = hash_object.hexdigest()
+    print(hex_dig)
 
-hash_object = hashlib.sha1(path)
-hex_dig = hash_object.hexdigest()
-print(hex_dig)
-
-print("MD5 Digest")
-
-hash_object = hashlib.md5(b'Hello World')
-print(hash_object.hexdigest())
+def MD5():
+    print("MD5 Digest")
+    hash_object = hashlib.md5(b'Hello World')
+    print(hash_object.hexdigest())
 
 
 print "End of functions"
