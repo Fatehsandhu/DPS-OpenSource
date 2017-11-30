ed6f262fe2691aa8955cb6921e4b8c7ea53b3658
Fatehsandhu <fatehsandhu@gmail.com>
2017-11-29 23:20:02 -0500

    Fix #1: Added SHA 1 Digest issue

--- Functions.py
+++ Functions.py
@@ -12,5 +12,12 @@ print last
 
 print os.path.getsize(path)
 
+print("Sha 1 Digest")
+
+hash_object = hashlib.sha1(path)
+hex_dig = hash_object.hexdigest()
+print(hex_dig)
+
+
 
 print "End of functions"
